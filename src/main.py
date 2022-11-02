cd40517c7364640faac0ef8e698a19b05efb6859
zaidannaufal <13518021@std.stei.itb.ac.id>
2022-11-02 20:00:38 +0700

    yg terakhir

--- src/main.py
+++ src/main.py
@@ -21,32 +21,31 @@ if __name__ == "__main__":
     ]
 
     matrix_images = list_img_to_matrix(folder_path)
-    # dummy_matrix = matrix_images[20]
 
-    # for layer in Layers:
-    #     dummy_matrix = layer.forward(dummy_matrix)
-    # print("Picture: ",folder_path[20])
-    # print("Predict Result :", class_dictionary[str(int(dummy_matrix[0]))])
+    for layer in Layers:
+        dummy_matrix = layer.forward(dummy_matrix)
+    print("Picture: ",folder_path[20])
+    print("Predict Result :", class_dictionary[str(int(dummy_matrix[0]))])
 
-    # X_train, X_test, y_train, y_test = train_test_split(matrix_images, class_label, test_size=0.1)
+    X_train, X_test, y_train, y_test = train_test_split(matrix_images, class_label, test_size=0.1)
 
-    # cnn = Model(Layers)
-    # cnn.fit(features=X_train, target=y_train, batch_size=5, epoch=3, learn_rate=0.1)
+    cnn = Model(Layers)
+    cnn.fit(features=X_train, target=y_train, batch_size=5, epoch=3, learn_rate=0.1)
 
-    # filename = "model1"
-    # cnn.save_model(filename)
+    filename = "model1"
+    cnn.save_model(filename)
 
-    # cnn2 = Model()
-    # cnn2.load_model(filename)
+    cnn2 = Model()
+    cnn2.load_model(filename)
 
-    # output = np.array([])
-    # for data in X_test:
-    #     forward_cnn = cnn2.forward(data)
-    #     output = np.append(output, np.rint(forward_cnn))
+    output = np.array([])
+    for data in X_test:
+        forward_cnn = cnn2.forward(data)
+        output = np.append(output, np.rint(forward_cnn))
     
-    # print("\nPredicted:", output)
-    # print("\nAccuracy:", metrics.accuracy_score(y_test, output))
-    # print("\nConfusion matrix:\n", metrics.confusion_matrix(y_test, output))
+    print("\nPredicted:", output)
+    print("\nAccuracy:", metrics.accuracy_score(y_test, output))
+    print("\nConfusion matrix:\n", metrics.confusion_matrix(y_test, output))
 
     kf = KFold(n_splits=10,shuffle=True)
     best_accuracy = 0
